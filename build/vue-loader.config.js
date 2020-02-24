29bce30eec603f0424e6ef44370e8a466c9d02e0
lesliele <1224622183@qq.com>
2020-02-24 15:05:41 +0800

    'vueinstance

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -8,33 +8,33 @@
  */
 // const docLoader = require.resolve('./doc-loader.js');
 module.exports = (isDev) => {
-    return {
-        // preserveWhitespace: false,//默认true，保留空格
-        // extractCSS: false,
-        // postcss,
-        // loaders: {
-        //     'docs': docLoader
-        // },
-        // preLoaders: {}, //解析前使用的loader
-        // postLoaders: {} //解析后使用的loader
-        // cssModules: {
-        //     localIdentName: '[path]-[name]-[hash:base64:5]',
-        //     camelCase: true
-        // }
-        // 下列选项已经被废弃了
-        // loader
-        // preLoaders
-        // postLoaders
-        // postcss
-        // cssSourceMap
-        // buble
-        // extractCSS
-        // template
+  return {
+    // preserveWhitespace: false,//默认true，保留空格
+    // extractCSS: false,
+    // postcss,
+    // loaders: {
+    //     'docs': docLoader
+    // },
+    // preLoaders: {}, //解析前使用的loader
+    // postLoaders: {} //解析后使用的loader
+    // cssModules: {
+    //     localIdentName: '[path]-[name]-[hash:base64:5]',
+    //     camelCase: true
+    // }
+    // 下列选项已经被废弃了
+    // loader
+    // preLoaders
+    // postLoaders
+    // postcss
+    // cssSourceMap
+    // buble
+    // extractCSS
+    // template
 
-        // 以下可以使用的v15
-        // hotReload: false //关闭热重载
-        // compilerOptions: {
-        //     preserveWhitespace: false //默认开启
-        // }
-    }
+    // 以下可以使用的v15
+    // hotReload: false //关闭热重载
+    // compilerOptions: {
+    //     preserveWhitespace: false //默认开启
+    // }
+  }
 }
--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -6,74 +6,75 @@
  * @Description: In User Settings Edit
  * @FilePath: \vue-ssr-tech\webpack.config.js
  */
-const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const vueLoaderOptions = require('./vue-loader.config.js');
-const isDev = process.env.NODE_ENV === 'development';
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const vueLoaderOptions = require('./vue-loader.config.js')
+const isDev = process.env.NODE_ENV === 'development'
 
 const config = {
-    entry: {
-        main: path.join(__dirname, '../client/main.js')
-    },
-    output: {
-        path: path.join(__dirname, '../dist'),
-        filename: '[name].[hash:8].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(vue|js|jsx)$/,
-                loader: 'eslint-loader',
-                exclude: /node_modules/,
-                enforce: 'pre'
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-                options: vueLoaderOptions(isDev)
-            },
-            {
-                resourceQuery: /blockType=docs/,
-                loader: require.resolve('./doc-loader.js')
-            },
-            {
-                test: /\.jsx$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(gif|jpg|jpeg|png|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1024,
-                            name: 'resources/[path][name].[ext]'
-                        }
-                    }
-                ]
-            }
+  mode: process.env.NODE_ENV || 'production',
+  entry: {
+    main: path.join(__dirname, '../client/main.js')
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].[hash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(vue|js|jsx)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: vueLoaderOptions(isDev)
+      },
+      {
+        resourceQuery: /blockType=docs/,
+        loader: require.resolve('./doc-loader.js')
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
         ]
-    },
-    plugins: [
-        new VueLoaderPlugin()
-    ],
-    resolve: {
-        alias: {
-            '@': path.join(__dirname, 'client')
-        }
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: 'resources/[path][name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'client')
     }
+  }
 }
 
-module.exports = config;
+module.exports = config
